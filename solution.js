3592611729aaf42732e67f9ec7aac75f34e9d7df
Omphile Morwane <omphilemorwane@gmail.com>
2024-06-28 15:20:08 +0200

    Added comments

--- solution.js
+++ solution.js
@@ -1,3 +1,8 @@
+// Initialize a loop with a variable 'line' starting as a single '#'
+// The loop continues as long as the length of 'line' is less than or equal to 7
+// After each iteration, add another '#' to 'line'
+
 for (let line = "#"; line.length <= 7; line += "#") {
+          // Log the current value of 'line' to the console
         console.log(line);
       }
